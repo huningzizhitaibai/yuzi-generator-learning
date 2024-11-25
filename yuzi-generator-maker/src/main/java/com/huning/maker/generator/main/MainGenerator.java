3b72f2056c67508ab823360537dbb8b1eab793d1
huning <afirstwhite@foxmail.com>
2024-11-25 22:27:55 +0800

    完成了jar包的打包工作

--- yuzi-generator-maker/src/main/java/com/huning/maker/generator/main/MainGenerator.java
+++ yuzi-generator-maker/src/main/java/com/huning/maker/generator/main/MainGenerator.java
@@ -3,6 +3,7 @@ package com.huning.maker.generator.main;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ClassPathResource;
 import cn.hutool.core.util.StrUtil;
+import com.huning.maker.generator.JarGenerator;
 import com.huning.maker.generator.file.DynamicFileGenerator;
 import com.huning.maker.meta.Meta;
 import com.huning.maker.meta.MetaManager;
@@ -13,7 +14,7 @@ import java.io.IOException;
 
 public class MainGenerator {
 
-    public static void main(String[] args) throws TemplateException, IOException {
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
         Meta meta = MetaManager.getMetaObject();
         System.out.println(meta);
 
@@ -40,6 +41,29 @@ public class MainGenerator {
         inputFilePath = inputResourcePath + File.separator + "templates/java/model/DataModel.java.ftl";
         outputFilePath = outputBaseJavaPackagePath + "/model/DataModel.java";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // generator.DynamicGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/DynamicGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/DynamicGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // generator.MainGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // generator.StaticGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/StaticGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/StaticGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // pom.xml
+        inputFilePath = inputResourcePath + File.separator + "templates/pom.xml.ftl";
+        outputFilePath = outputPath + File.separator + "pom.xml";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // 构建 jar 包
+        JarGenerator.doGenerate(outputPath);
     }
 }
 
--- yuzi-generator-maker/src/main/java/com/huning/maker/meta/Meta.java
+++ yuzi-generator-maker/src/main/java/com/huning/maker/meta/Meta.java
@@ -8,6 +8,7 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 public class Meta {
+
     private String name;
     private String description;
     private String basePackage;
@@ -23,11 +24,11 @@ public class Meta {
         private String inputRootPath;
         private String outputRootPath;
         private String type;
-        private List<FileInfo> files;
+        private List<Files> files;
 
         @NoArgsConstructor
         @Data
-        public static class FileInfo {
+        public static class Files {
             private String inputPath;
             private String outputPath;
             private String type;
@@ -35,19 +36,18 @@ public class Meta {
         }
     }
 
-    //有一步修改ModelInfo.defaultValue的类型为Object实在是不知道在说哪
     @NoArgsConstructor
     @Data
     public static class ModelConfig {
-        private List<ModelInfo> models;
+        private List<Models> models;
 
         @NoArgsConstructor
         @Data
-        public static class ModelInfo {
+        public static class Models {
             private String fieldName;
             private String type;
             private String description;
-            private Boolean defaultValue;
+            private Object defaultValue;    //一定要注意将这里的defaultValue的类型改为Object，不然所有的默认都会是Boolean
             private String abbr;
         }
     }
